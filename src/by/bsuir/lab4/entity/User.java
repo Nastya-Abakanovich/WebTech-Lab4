f067d0e626c5cf05e01eee28e708c1653f609a63
Nastya-Abakanovich <nastya.abakanovich@gmail.com>
2022-12-11 14:28:54 +0300

    Created entities for rooms, users and orders.

---
+++ src/by/bsuir/lab4/entity/User.java
@@ -0,0 +1,63 @@
+package by.bsuir.lab4.entity;
+
+public class User {
+    private int id;
+    private String login;
+    private String password;
+    private int role;
+
+    public User(String login, String password, int role) {
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public User(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
